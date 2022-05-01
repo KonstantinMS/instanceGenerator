d241749f4077f9a065aa861387be7c132aa5d079
KonstantinMS <konstantinms@ya.ru>
2022-05-01 22:55:16 +0700

    Сделал парсер для верилога по простому шаблону
    module name ( input in, output out);

--- TestModuleParser.py
+++ TestModuleParser.py
@@ -4,30 +4,35 @@ import moduleParser
 class TestModuleParser(unittest.TestCase):
     def setUp(self):
         self.SimpleString = \
-        '''
-        module module (
-          input 	      i_CLK_13m5,    	//  serial clock 
-          input 	      i_RST,          	//  hight for reset
-          input           i_EN,             //  system i_ENable
-          //  Y8                 
-          input  [ 7 : 0] i_Y_data,         //  data in
-        
-          
-          output [ 9 : 0] PIX_CNT_o,        //  pix count.
-          output [ 9 : 0] LINE_CNT_o,       //  Line count
-          output          VSYNC_o,          //  vertical synchronization
-          output          HSYNC_o,          //  horizontal synchronization 
-          output          DATA_RQ_o,        //  active area 
-        
-          output [ 7 : 0] Y_data_o          //  parallel output            
-        );
-        other tings text
-        reg dsfasf;
-        wire [0:600] dfasf; 
-        endmodule
+        '''module Y8 (
+  output 	      i_CLK_13m5,    	//  serial clock
+  input 	      i_RST,          	//  hight for reset
+  input           i_EN,             //  system i_ENable
+  //  module Y8
+  input  [7:0] i_Y_data,         //  data in
+
+  output [ 9 : 0] PIX_CNT_o,   //  pix count.
+  output [ 9 : 0] LINE_CNT_o,       //  Line count,
+  output          VSYNC_o,          //  vertical synchronization
+  output          HSYNC_o,          //  horizontal synchronization
+  inout          DATA_RQ_o,        //  active area
+
+  output [ 7 : 0] Y_data_o          //  parallel output
+);
+
+reg simpleREG;
+wire [0:600] someWire; // comment
+//block comment
+
+/* input reg commetnReg;
+// comment
+assign ww = PIX_CNT_o ^ LINE_CNT_o;
+*/
+
+endmodule
         '''
         self.SimpleStringResult = \
-            '''module module_0 (
+            '''module Y8_0 (
 .i_CLK_13m5 (  ),
 .i_RST      (  ),
 .i_EN       (  ),
--- moduleParser.py
+++ moduleParser.py
@@ -1,51 +1,93 @@
 # Парсинг
 import re
 
+# добавим пробелы после символов (),
+# s=''.join((' {} '.format(el) if el in '(),' else el for el in ModuleString))
+# преобразуем в список, чтобы далее разбить на слова
 
-s =''
-'''
-module Y8 (
-  input 	      i_CLK_13m5,    	//  serial clock
+s ='''module Y8 (
+  output 	      i_CLK_13m5,    	//  serial clock
   input 	      i_RST,          	//  hight for reset
   input           i_EN,             //  system i_ENable
-  //  Y8
-  input  [ 7 : 0] i_Y_data,         //  data in
+  //  module Y8
+  input  [7:0] i_Y_data,         //  data in
 
-
-  output [ 9 : 0] PIX_CNT_o,        //  pix count.
-  output [ 9 : 0] LINE_CNT_o,       //  Line count
+  output [ 9 : 0] PIX_CNT_o,   //  pix count.
+  output [ 9 : 0] LINE_CNT_o,       //  Line count,
   output          VSYNC_o,          //  vertical synchronization
   output          HSYNC_o,          //  horizontal synchronization
-  output          DATA_RQ_o,        //  active area
+  inout          DATA_RQ_o,        //  active area
 
   output [ 7 : 0] Y_data_o          //  parallel output
 );
-other tings text
-reg dsfasf;
-wire [0:600] dfasf;
+
+reg simpleREG;
+wire [0:600] someWire; // comment
+//block comment
+
+/* input reg commetnReg;
+// comment
+assign ww = PIX_CNT_o ^ LINE_CNT_o;
+*/
+
 endmodule
 '''
-
 def getInstance(s, postfix ='_0'):
     # пустая строка
     if s == '':
         return f'Введена пустая строка'
-    # оставим только содержимое модуля
-    ModuleString = s[s.find('module') : s.find(');')+2]
-    #print('Содержимое модуля: ', ModuleString)
-    # добавим пробелы после символов (),
-    #s=''.join((' {} '.format(el) if el in '(),' else el for el in ModuleString))
-    # преобразуем в список, чтобы далее разбить на слова
+    s = deleteComments(s)
+    # определим язык, verilog vs VHDL
+    language = identifyProgLang(s)
 
-    # запомним имя модуля
-    ModuleName = ModuleString[ ModuleString.find('module') + 7: ModuleString.find('(') - 1 ].strip()
-    # если не нашлось модуля
-    if ModuleName == '':
-        ModuleName = 'ModuleName' #Не смог найти имя модуля, может быть так и зажумано'
+    if language == 'verilog':
+        # запомним имя модуля
+        ModuleName = s[s.find('module') + 7: s.find('(') - 1].strip()
+        print(ModuleName)
+        # если скобки содержат описания направления портов
+        if re.match(r'(.*?)[input|output|inout] (.*?)\);', s, flags=re.S):
+            ports = parseVerilogPatternSimple(s)
+        else:
+            ports = parseVerilogPatternHard(s)
 
-    print('имя модуля: ', ModuleName)
+    # формируем результат
+    # имя
+    out = 'module' + ' ' + ModuleName + postfix+' (\n'
+    try:
+        # переменные
+        maxLen = len( max( ports, key=len ) )
+        for i, item in enumerate(ports):
+            out += ('.' + item.ljust(maxLen, ' ') + ' (  )')
+            if i != len(ports)-1:
+                out += ',\n'
+        print('\n')
+        out += '\n);'
+    except:
+        out += 'Не нашел ни одного порта);'
+    # закрывающая строка
+    print(out)
+    return out
 
-    remove_list = ['input','output', 'reg', 'wire', 'int', 'bit', 'logic', 'packet_t', ',']
+
+def deleteComments (s):
+    # удалим все комментарии // и /* */
+    return re.sub('//.*?\n|/\*.*?\*/', '', s, flags=re.S)
+
+
+def identifyProgLang (s):
+    if re.search(r'module.*?\(', s, flags=re.S):
+        return 'verilog'
+    elif re.search(r'entity.*?is', s, flags=re.S):
+        return 'VHDL'
+    else:
+        return None
+
+
+def parseVerilogPatternSimple(s):
+    # получим содержание скобок модуля
+    ModuleString = s[s.find('module'): s.find(');') + 2]
+
+    remove_list = ['input', 'output', 'inout', 'reg', 'wire', 'int', 'bit', 'logic', 'packet_t', ',']
     ports = []
     # разбиваем на строки по запятой и делим на строки
     d = ModuleString.replace(",", ",\n")
@@ -54,8 +96,8 @@ def getInstance(s, postfix ='_0'):
     InputCount, OutputCount = 0, 0
 
     for line in lines:
-        result1 = re.match(r'(.+)[input|output] (.+),', line)
-        result2 = re.match(r'(.+)[input|output] (.+)', line)
+        result1 = re.match(r'(.+)[input|output|inout] (.+),', line)
+        result2 = re.match(r'(.+)[input|output|inout] (.+)', line)
         if result1 is not None:
             # удалить [*]
             line = [re.sub(r'\[[^\[\]]*\]\ ', '', line)]
@@ -74,25 +116,11 @@ def getInstance(s, postfix ='_0'):
             last = last.split()
     if 'last' in locals():
         ports.append(last[0])
-    print('inputs:', ports)
+    return ports
 
-    # имя
-    out = ModuleName + ' ' + ModuleName + postfix+' (\n'
-    try:
-        # переменные
-        maxLen = len( max( ports, key=len ) )
-        for i, item in enumerate(ports):
-            out += ('.' + item.ljust(maxLen, ' ') + ' (  )')
-            if i != len(ports)-1:
-                out += ',\n'
-        print('\n')
-        out += '\n);'
-    except:
-        out += 'Не нашел ни одного порта);'
-    # закрывающая строка
-    #print(out)
-    return out
+def parseVerilogPatternHard(s):
 
+    pass
 
 if __name__ == "__main__":
     getInstance(s)
